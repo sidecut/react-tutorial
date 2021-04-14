d4f0b3da002337c541ee2889e2bc9afe9328c44f
Jim Raden <james.raden@gmail.com>
2021-04-14 11:45:20 -0400

    Add prop

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 // @ts-check
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 class Square extends React.Component {
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return <button className="square">{this.props.value}</button>;
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
